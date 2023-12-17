cfa58f173d39ee1507f5c5e57bad942e9d402afb
SDKishor <kishorsutradhar32@gmail.com>
2023-12-17 21:30:50 +0600

    problem 1 solved

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,12 +1,45 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [tableData, setTableData] = useState([]);
+    const [fillterData, setFillterData] = useState([]);
+    const nameRef = useRef("")
+    const statusRef = useRef("")
+
 
     const handleClick = (val) =>{
         setShow(val);
+        if(val == "active"){
+            const result = tableData.filter((data) => data.status === "active");
+        setFillterData(result);
+        }
+        if(val == "completed"){
+            const result = tableData.filter((data) => data.status === "completed");
+        setFillterData(result);
+        }
+        if(val == "all"){
+            
+        setFillterData("");
+        }
+        
     }
+    
+
+    
+    const handleSubmitData = (e) =>{
+        e.preventDefault();
+        const newdata = {name:nameRef.current.value, status:statusRef.current.value}; 
+        const newtabledata = [...tableData, newdata];
+        const sortedData = newtabledata.sort((a, b) => a.status.localeCompare(b.status));
+        setTableData(sortedData)
+        e.target[0].value = "";
+        e.target[1].value = "";
+        
+        
+    }
+    
 
     return (
 
@@ -14,12 +47,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form onSubmit={handleSubmitData} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" ref={nameRef} required  className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" ref={statusRef} required className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -47,7 +80,21 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                        {
+                            show == "all" ? tableData.map((data, index) =>(
+                                <tr key={index}>
+                            <th scope="col">{data.name}</th>
+                            <th scope="col">{data.status}</th>
+                            </tr>)) :
+
+                            fillterData.map((data, index) =>(
+                                <tr key={index}>
+                            <th scope="col">{data.name}</th>
+                            <th scope="col">{data.status}</th>
+                            </tr>))
+                            
+                            }
+                            
                         </tbody>
                     </table>
                 </div>
